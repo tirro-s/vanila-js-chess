e621c85dd16a5dbe7d27af450363f97428d9f277
Tirro <tirro-s@ukr.net>
2025-09-18 18:25:42 +0300

    move piece

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
+let boardNotes;
+let activeItem;
+
 function init() {
   const board = document.querySelector('#board');
 
+  let savedNode;
+
   for(let i = 0; i < 64; i++) {
     const node = document.createElement("div");
-    node.innerText = i;
+    // node.innerText = i;
     node.classList.add('cell');
+    node.setAttribute('custom-id', i);
+    node.onclick = clickFunc;
+
+    if (i === 28) {
+      savedNode = node;
+    }
 
     const row = Math.floor(i / 8);
     
@@ -14,6 +25,40 @@ function init() {
     board.appendChild(node);
   }
 
+  boardNotes = document.querySelectorAll('.cell');
+
+  const image = document.createElement('img');
+  image.setAttribute('src', 'images/piaces/knight.png');
+  savedNode.appendChild(image);
+
+}
+
+function clickFunc(event) {
+  const position = getCellPosition(event);
+  if (activeItem) {
+    activeItem.cell.classList.remove('active');
+    const node = boardNotes[position];
+    node.appendChild(activeItem.piece);
+    activeItem = null;
+  } else {
+    const node = boardNotes[position];
+    if (node.childNodes.length) {
+      node.classList.add('active');
+      activeItem = {
+        position: position,
+        cell: node,
+        piece: node.childNodes[0]
+      }
+    }
+  }
+}
+
+function getCellPosition(event) {
+  target = event.target;
+  if (target.nodeName === 'IMG') {
+    target = target.parentNode;
+  }
+  return target.getAttribute('custom-id');
 }
 
 init();
